74ebeec1b86ff6ad9aa1cf5b4d0b310939b959e2
Vishesh <visheshsingh16@gmail.com>
2017-04-28 20:40:42 -0400

    added links for repo page

--- js/main.js
+++ js/main.js
@@ -24,13 +24,15 @@ $(document).ready(function(){
             <div class="well">
               <div class="row">
                 <div class="col-md-7">
-                  <strong>${repo.name}</strong>
+                  <strong>${repo.name}</strong>: ${repo.description}
                 </div>
                 <div class="col-md-3">
-
+                  <span class="label label-default">Forks: ${repo.forks_count}</span>
+                  <span class="label label-primary">Gists: ${repo.watchers_count}</span>
+                  <span class="label label-success">Stars: ${repo.stargazers_count}</span>
                 </div>
                 <div class="col-md-2">
-
+                  <a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo page</a>
                 </div>
               </div>
             </div>
